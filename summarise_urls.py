51db2f51a6cc249e4e344b9f14f2089355c49fa9
Archit Singh <131599737+archit15singh@users.noreply.github.com>
2023-11-02 03:20:20 +0530

    Update summarise_urls.py

--- summarise_urls.py
+++ summarise_urls.py
@@ -14,136 +14,137 @@ import time
 
 
 def save_result_to_file(result, filename):
-  with open(filename, 'w', encoding='utf-8') as file:
-    file.write(result)
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(result)
 
 
 def create_or_empty_folder(folder_path):
-  if not os.path.exists(folder_path):
-    os.makedirs(folder_path)
-  else:
-    for filename in os.listdir(folder_path):
-      file_path = os.path.join(folder_path, filename)
-      if os.path.isfile(file_path):
-        os.unlink(file_path)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    else:
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
 
 
 def get_cpu_ram_usage():
-  cpu_percent = psutil.cpu_percent()
-  ram_percent = psutil.virtual_memory().percent
-  return cpu_percent, ram_percent
+    cpu_percent = psutil.cpu_percent()
+    ram_percent = psutil.virtual_memory().percent
+    return cpu_percent, ram_percent
 
 
 def print_usage_info():
-  cpu_percent, ram_percent = get_cpu_ram_usage()
-  print(f"CPU Usage: {cpu_percent}% | RAM Usage: {ram_percent}%")
+    cpu_percent, ram_percent = get_cpu_ram_usage()
+    print(f"CPU Usage: {cpu_percent}% | RAM Usage: {ram_percent}%")
 
 
 def scrape_links(url):
-  try:
-    response = requests.get(url)
-    if response.status_code == 200:
-      soup = BeautifulSoup(response.text, 'html.parser')
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-      links = soup.find_all('a')
-      scraped_links = []
+            links = soup.find_all('a')
+            scraped_links = []
 
-      for link in links:
-        href = link.get('href')
-        if href:
-          absolute_url = urljoin(url, href)
+            for link in links:
+                href = link.get('href')
+                if href:
+                    absolute_url = urljoin(url, href)
 
-          parsed_absolute_url = urlparse(absolute_url)
-          parsed_base_url = urlparse(base_url)
+                    parsed_absolute_url = urlparse(absolute_url)
+                    parsed_base_url = urlparse(base_url)
 
-          if parsed_absolute_url.netloc == parsed_base_url.netloc:
-            scraped_links.append(absolute_url)
+                    if parsed_absolute_url.netloc == parsed_base_url.netloc:
+                        scraped_links.append(absolute_url)
 
-      return scraped_links
+            return scraped_links
 
-  except Exception as e:
-    print(f"Error: {str(e)}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
 
-  return []
+    return []
 
 
 def scrape_links_iteration(start_url, max_depth, data_folder):
-  visited = set()
-  to_visit = deque([(start_url, 0)])
-  scraped_links = []
+    visited = set()
+    to_visit = deque([(start_url, 0)])
+    scraped_links = []
 
-  while to_visit:
-    current_url, depth = to_visit.popleft()
+    while to_visit:
+        current_url, depth = to_visit.popleft()
 
-    if depth > max_depth:
-      continue
+        if depth > max_depth:
+            continue
 
-    if current_url in visited:
-      continue
+        if current_url in visited:
+            continue
 
-    try:
-      links = scrape_links(current_url)
-      tqdm_links = tqdm(links, desc=f"Scraping {current_url}")
-      for link in tqdm_links:
-        scraped_links.append(link)
-        to_visit.append((link, depth + 1))
-        visited.add(link)
+        try:
+            links = scrape_links(current_url)
+            tqdm_links = tqdm(links, desc=f"Scraping {current_url}")
+            for link in tqdm_links:
+                scraped_links.append(link)
+                to_visit.append((link, depth + 1))
+                visited.add(link)
 
-    except Exception as e:
-      print(f"Error: {str(e)}")
+        except Exception as e:
+            print(f"Error: {str(e)}")
 
-  return scraped_links
+    return scraped_links
 
 
 def process_url(url, data_folder):
-  try:
-    print(f"Processing URL: {url}")
-    loader = WebBaseLoader(url)
-    docs = loader.load()
-    print(f"length: {len(docs[0].page_content)} for url: {url}")
+    try:
+        print(f"Processing URL: {url}")
+        loader = WebBaseLoader(url)
+        docs = loader.load()
+        print(f"length: {len(docs[0].page_content)} for url: {url}")
 
-    llm = Ollama(model="llama2")
-    chain = load_summarize_chain(llm, chain_type="stuff")
-    result = chain.run(docs)
-    print(f"Length of result for {url}: {len(result)}")
-    result_filename = os.path.join(data_folder,
-                                   f"{url.replace('/', '_')}_result.txt")
-    save_result_to_file(result, result_filename)
-  except Exception as e:
-    print(str(e))
+        llm = Ollama(model="llama2")
+        chain = load_summarize_chain(llm, chain_type="stuff")
+        result = chain.run(docs)
+        print(f"Length of result for {url}: {len(result)}")
+        result_filename = os.path.join(data_folder,
+                                       f"{url.replace('/', '_')}_result.txt")
+        save_result_to_file(result, result_filename)
+    except Exception as e:
+        print(str(e))
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--base_url",
-                      type=str,
-                      required=True,
-                      help="Base URL to start scraping from")
-  parser.add_argument("--max_depth",
-                      type=int,
-                      required=True,
-                      help="Maximum depth for recursion")
-  parser.add_argument("--data_folder",
-                      type=str,
-                      required=True,
-                      help="Folder to save results")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--base_url",
+                        type=str,
+                        required=True,
+                        help="Base URL to start scraping from")
+    parser.add_argument("--max_depth",
+                        type=int,
+                        required=True,
+                        help="Maximum depth for recursion")
+    parser.add_argument("--data_folder",
+                        type=str,
+                        required=True,
+                        help="Folder to save results")
+    args = parser.parse_args()
 
-  base_url = args.base_url
-  max_depth = args.max_depth
-  start_url = base_url
-  data_folder = args.data_folder
+    base_url = args.base_url
+    max_depth = args.max_depth
+    start_url = base_url
+    data_folder = args.data_folder
 
-  create_or_empty_folder(data_folder)  # Create or empty the data folder
+    create_or_empty_folder(data_folder)  # Create or empty the data folder
 
-  unique_scraped_links = scrape_links_iteration(start_url, max_depth,
-                                                data_folder)
-  print(
-      f"Found {len(unique_scraped_links)} final links from base URL: {base_url}"
-  )
+    unique_scraped_links = scrape_links_iteration(start_url, max_depth,
+                                                  data_folder)
+    print(
+        f"Found {len(unique_scraped_links)} final links from base URL: {base_url}"
+    )
 
-  while unique_scraped_links:
-    link = unique_scraped_links.pop(0)
-    print('*' * 100)
-    process_url(link, data_folder)
-    print_usage_info()  # Print CPU and RAM usage
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        for link in tqdm(unique_scraped_links):
+            executor.submit(process_url, link, data_folder)
+            print('*' * 100)
+            print_usage_info()  # Print CPU and RAM usage
+            time.sleep(5)
