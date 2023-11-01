6484f1226d47f86b0de5c5d04c18000ad6f133fb
Archit Singh <131599737+archit15singh@users.noreply.github.com>
2023-11-02 03:09:58 +0530

    Update summarise_urls.py

--- summarise_urls.py
+++ summarise_urls.py
@@ -8,6 +8,7 @@ import os
 from langchain.llms import Ollama
 from langchain.document_loaders import WebBaseLoader
 from langchain.chains.summarize import load_summarize_chain
+from concurrent.futures import ThreadPoolExecutor
 
 def save_result_to_file(result, filename):
     with open(filename, 'w', encoding='utf-8') as file:
@@ -22,44 +23,60 @@ def create_or_empty_folder(folder_path):
             if os.path.isfile(file_path):
                 os.unlink(file_path)
 
+def scrape_links(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            links = soup.find_all('a')
+            scraped_links = []
+
+            for link in links:
+                href = link.get('href')
+                if href:
+                    absolute_url = urljoin(url, href)
+
+                    parsed_absolute_url = urlparse(absolute_url)
+                    parsed_base_url = urlparse(base_url)
+
+                    if parsed_absolute_url.netloc == parsed_base_url.netloc:
+                        scraped_links.append(absolute_url)
+
+            return scraped_links
+
+    except Exception as e:
+        print(f"Error: {str(e}")
+
+    return []
+
 def scrape_links_iteration(start_url, max_depth, data_folder):
     visited = set()
     to_visit = deque([(start_url, 0)])
     scraped_links = []
 
-    while to_visit:
-        current_url, depth = to_visit.popleft()
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        while to_visit:
+            current_url, depth = to_visit.popleft()
 
-        if depth > max_depth:
-            continue
+            if depth > max_depth:
+                continue
 
-        if current_url in visited:
-            continue
+            if current_url in visited:
+                continue
 
-        try:
-            response = requests.get(current_url)
-            if response.status_code == 200:
-                soup = BeautifulSoup(response.text, 'html.parser')
-
-                links = soup.find_all('a')
+            try:
+                links = executor.map(scrape_links, [current_url])
+                links = list(set([url for sublist in links for url in sublist]))
                 tqdm_links = tqdm(links, desc=f"Scraping {current_url}")
                 for link in tqdm_links:
-                    href = link.get('href')
-                    if href:
-                        absolute_url = urljoin(current_url, href)
-                        
-                        parsed_absolute_url = urlparse(absolute_url)
-                        parsed_base_url = urlparse(base_url)
-                        
-                        if parsed_absolute_url.netloc == parsed_base_url.netloc:
-                            scraped_links.append(absolute_url)
-                            to_visit.append((absolute_url, depth + 1))
-                            visited.add(absolute_url)
+                    scraped_links.append(link)
+                    to_visit.append((link, depth + 1))
+                    visited.add(link)
 
-        except Exception as e:
-            print(f"Error: {str(e)}")
+            except Exception as e:
+                print(f"Error: {str(e}")
 
-    scraped_links = list(set(scraped_links))
     return scraped_links
 
 def process_url(url, data_folder):
@@ -68,7 +85,7 @@ def process_url(url, data_folder):
         loader = WebBaseLoader(url)
         docs = loader.load()
         print(f"length: {len(docs[0].page_content)} for url: {url}")
-    
+
         llm = Ollama(model="llama2")
         chain = load_summarize_chain(llm, chain_type="stuff")
         result = chain.run(docs)
@@ -76,7 +93,7 @@ def process_url(url, data_folder):
         result_filename = os.path.join(data_folder, f"{url.replace('/', '_')}_result.txt")
         save_result_to_file(result, result_filename)
     except Exception as e:
-        print(str(e))
+        print(str(e)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -95,6 +112,6 @@ if __name__ == "__main__":
     unique_scraped_links = scrape_links_iteration(start_url, max_depth, data_folder)
     print(f"Found {len(unique_scraped_links)} final links from base URL: {base_url}")
 
-    for link in tqdm(unique_scraped_links, desc="Processing Links"):
-        print('*'*100)
-        process_url(link, data_folder)
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        for link in tqdm(unique_scraped_links, desc="Processing Links"):
+            executor.submit(process_url, link, data_folder)
